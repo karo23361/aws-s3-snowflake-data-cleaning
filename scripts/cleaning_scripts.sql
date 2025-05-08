2a1dcbc86914d33b8ba53fd8e63cc4cbd887f1e8
Karol Szeląg <118350099+karo23361@users.noreply.github.com>
2025-05-08 11:49:56 +0200

    Update cleaning_scripts.sql

--- scripts/cleaning_scripts.sql
+++ scripts/cleaning_scripts.sql
@@ -1,8 +1,12 @@
---The Main structure of main SQL query 
+--Main SELECT
 
 SELECT
 full_name,
-age,
+CASE 
+    WHEN age < 0 THEN null
+    WHEN age > 100 THEN null
+    ELSE age
+    END as age,
 CASE 
     WHEN martial_status is null THEN 'n/a'
     WHEN martial_status = 'divored' THEN 'divorced'
@@ -38,4 +42,41 @@ SELECT CASE
     ELSE martial_status
     END as martial_status
 FROM club_members;
+
+
+--Testing Age
+SELECT age FROM club_members WHERE age <0 OR age > 100 OR age IS NULL; -- Discovered few nulls and values above 100
+
+WITH age_test as (
+SELECT CASE 
+    WHEN age < 0 THEN null
+    WHEN age > 100 THEN null
+    ELSE age
+    END as age
+FROM CLUB_MEMBERS) 
+SELECT age from age_test where age > 100 OR age < 0;
+
+
+--Cleaing Age
+SELECT CASE 
+    WHEN age < 0 THEN null
+    WHEN age > 100 THEN null
+    ELSE age
+    END as age
+FROM CLUB_MEMBERS;
+
+
+--Testing email
+
+SELECT email FROM club_members WHERE email != trim(email);
+SELECT email
+FROM club_members 
+    WHERE 
+    email not like '%@%.%' OR
+    email like '% %' OR 
+    email like '%,%' OR 
+    email like '%.@%';
+
+--Cleaning Emial - Unecessery
+
 
